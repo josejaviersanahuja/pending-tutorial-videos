9d84bfb5334f5c1fca6ca24753724cb65a8e0784
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2022-05-14 13:50:31 +0100

    refactorizando stilos header

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,14 +1,12 @@
 import { User } from 'firebase/auth'
 import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { authStateChanged } from '../firebase/auth'
 import { ILoginUser, IUser } from '../interfaces'
 
 export default function useUser () {
   const [loginUser, setLoginUser] = useState<ILoginUser | null | undefined>(null)
   const [user, setUser] = useState<IUser | null | undefined>(null)
-  const navigate = useNavigate()
-
+ 
   const twoFunctionsInOne = (loginUser : User | null | undefined ) => { 
     if (loginUser !== null && loginUser !== undefined) {
         const {displayName, email, photoURL, refreshToken,metadata, uid } = loginUser
@@ -27,12 +25,6 @@ export default function useUser () {
          authStateChanged(twoFunctionsInOne)
    }, [])
 
-
-  useEffect(() => {
-    console.log('2 ', loginUser);
-    
-    if(!loginUser) navigate('/login')
-  }, [loginUser])
 
   return { user, loginUser}
 }
