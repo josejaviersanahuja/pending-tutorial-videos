16d51fd4d4bea44547496c774b3b0e1c0d9ff0a4
José Javier Sanahuja <josejaviersanahuja@gmail.com>
2022-05-15 04:03:19 +0100

    empezamos a usar firestore

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import Avatar from "../../components/Avatar";
 import useUser from "../../hooks/useUser";
 import HomeLoginBtn from "./HomeLoginBtn";
 
@@ -7,15 +8,20 @@ interface IHome {
 
 export default function Home({}:IHome) {
 
+  const {user} = useUser()
+  
   return (
     <div className="home__page">
       <header>
+        <Avatar />
         <h1>Videos Aún Por Ver</h1>
         <HomeLoginBtn />
       </header>
       <main>
         <h2>Esto es la Home</h2>
-        
+        { user === undefined && <p>undefined</p>}
+        { user === null && <p>null</p>}
+        { user && <p>{user.uid}</p>}
       </main>
     </div>
   );
