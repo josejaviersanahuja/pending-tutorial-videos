1a4fb4d86fefc19a885f3c3cf4854e9d11ea1978
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2022-06-03 00:52:41 +0100

    proyecto rescatado, y claves ocultas nuevamente

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
+ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
-root.render(
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
